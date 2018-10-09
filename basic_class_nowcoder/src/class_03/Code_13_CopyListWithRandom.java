27e66f3c5f6b808a592682cb945c8feefc75ad28
JasonZhangCauc <hasp_Jason@163.com>
2018-10-09 22:23:51 +0800

    实现一个特殊的栈，在实现栈的基本功能的基础上，再实现返回栈中最小元素的操作。
    Signed-off-by: JasonZhangCauc <hasp_Jason@163.com>

--- basic_class_nowcoder/src/class_03/Code_13_CopyListWithRandom.java
+++ basic_class_nowcoder/src/class_03/Code_13_CopyListWithRandom.java
@@ -31,14 +31,17 @@ public class Code_13_CopyListWithRandom {
 	}
 
 	public static Node copyListWithRand1(Node head) {
+		
 		HashMap<Node, Node> map = new HashMap<Node, Node>();
 		Node cur = head;
+		
 		while (cur != null) {
 			map.put(cur, new Node(cur.value));
 			cur = cur.next;
 		}
 		cur = head;
 		while (cur != null) {
+			
 			map.get(cur).next = map.get(cur.next);
 			map.get(cur).rand = map.get(cur.rand);
 			cur = cur.next;
